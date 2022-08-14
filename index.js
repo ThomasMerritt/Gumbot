0c943a01849ee86646237fd6c72bf1049424eebc
ThomasMerritt <84432380+ThomasMerritt@users.noreply.github.com>
2022-08-14 13:08:56 -0700

    The Bot

---
+++ index.js
@@ -0,0 +1,74 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+require('dotenv').config();
+var Scraper = require('images-scraper');
+
+client.login(process.env.BOTTOKEN);
+client.on('ready', readyDiscord);
+
+function readyDiscord() {
+    console.log('on');
+}
+
+client.on('message', getMessage);
+
+const PREFIX = '!';
+
+const responses = [
+    '*Blow Kiss*',
+    'No',
+    '*Yacks*',
+    'Do I need to?',
+    'Go away'
+];
+
+
+async function getMessage(msg) {
+    let args = msg.content.substring(PREFIX.length).split(' ');
+    const Guild = client.guilds.cache.get('983067222144602132');
+    const Members = Guild.members.cache.map(member => member.user.username);    
+    console.log(Members);
+
+    switch (args[0]) {
+        case 'love':
+            msg.reply(responses[Math.floor(Math.random() * responses.length)]);
+            break;
+        case 'slap':
+            if (Members.includes(args[1])) {
+                    msg.reply(`slapped ${args[1]}`);
+                    grabImage('anime slap gif');
+            } else {
+                msg.reply('Slap missed');
+            }
+            
+        case 'bleach':
+            if (!args[1]) {
+                return msg.reply('Please define the amount to clear');
+            } else {
+                msg.channel.bulkDelete(args[1]);
+            }
+            break;
+        case 'image':
+            grabImage(args.join(' '));
+            break;
+        case 'url':
+            let avatarUrl = msg.author.displayAvatarURL();
+            msg.channel.send(avatarUrl);
+    }
+
+    function grabImage(image_query) {
+        const google = new Scraper({
+            puppeteer: {
+              headless: true,
+            },
+          })
+    
+          console.log('yes');
+    
+          (async () => {
+                console.log(image_query.length);
+                const results = await google.scrape(image_query, 100);
+                msg.channel.send(results[Math.floor(Math.random() * results.length)].url);
+          })();
+    }
+}
